b61c25e9f4896f44bbe31b431b9095692d3b416f
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-29 11:36:12 +0100

    add page the vote

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import Page from './components/Page';
 
 function App() {
+
   return (
     <div className="App">
+       <h1>Popular Apps on Product Hunt</h1>
+      <Page/>
+      <Page/>
+      <Page/>
       <Page/>
     </div>
   );
--- mon-app/src/components/Page.jsx
+++ mon-app/src/components/Page.jsx
@@ -18,8 +18,7 @@ const incrementer = () => {
 
   return (
     <>
-        <h1>Popular Apps on Product Hunt</h1>
-            <hr />
+
             <div className='text-center justify-content-center d-flex'>
                 <div className="card mb-3" style={{maxWidth:"500px"}}>
                     <div className="row g-0">
@@ -36,54 +35,7 @@ const incrementer = () => {
                     </div>
                 </div>
             </div>
-            <div className='text-center justify-content-center d-flex'>
-                <div className="card mb-3" style={{maxWidth:"500px"}}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="..." className="img-fluid rounded-start" alt="..."/>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                            <h5 className="card-title text-start "><button ><i className="bi bi-caret-up-fill"></i></button><span className='ms-2'><i className="bi bi-caret-down-fill"></i></span> <span  className='ms-3'></span></h5>
-                                <p className="card-text text-start "><a className='' href="#">Tinfolid: tailored tinfoil hats</a> <br/> below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text text-start"><span className='fw-bold'>Submitted by :</span> <img className='img' src="/image/person2.png" alt="" /></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='text-center justify-content-center d-flex'>
-                <div className="card mb-3" style={{maxWidth:"500px"}}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="..." className="img-fluid rounded-start" alt="..."/>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                            <h5 className="card-title text-start "><i className="bi bi-caret-up-fill"></i><span className='ms-2'><i className="bi bi-caret-down-fill"></i></span></h5>
-                                <p className="card-text text-start "><a className='' href="#">Tinfolid: tailored tinfoil hats</a> <br/> below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text text-start"><span className='fw-bold'>Submitted by :</span> <img className='img' src="/image/person2.png" alt="" /></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='text-center justify-content-center d-flex'>
-                <div className="card mb-3" style={{maxWidth:"500px"}}>
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <img src="..." className="img-fluid rounded-start" alt="..."/>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
-                            <h5 className="card-title text-start "><i className="bi bi-caret-up-fill"></i><span className='ms-2'><i className="bi bi-caret-down-fill"></i></span> </h5>
-                                <p className="card-text text-start "><a className='' href="#">Tinfolid: tailored tinfoil hats</a> <br/> below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text text-start"><span className='fw-bold'>Submitted by :</span> <img className='img' src="/image/person2.png" alt="" /></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    
     </>
   )
 }
